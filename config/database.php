678da9e72b6c41a1f916894353311ca0ac54ea0d
lightbrca1z <quwieorpt@gmail.com>
2025-08-01 14:34:03 +0900

    health-app backend 20250801-14:34

--- config/database.php
+++ config/database.php
@@ -40,6 +40,7 @@ return [
             'busy_timeout' => null,
             'journal_mode' => null,
             'synchronous' => null,
+            'strict' => false,
         ],
 
         'mysql' => [
